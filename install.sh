21a5862d22146f4be8add66b882e7a31b87f4dd8
VixKacha <viktor.kacavenda@gmail.com>
2025-02-14 13:06:43 +0100

    sc

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@ echo "Installing packages..."
 sudo pacman -S --needed hyprland waybar rofi hyprpaper hyprlock wlogout neofetch kitty NetworkManager alsa-utils pavucontrol cmatrix neovim kwrite thunar base-devel git fftw alsa-lib iniparser pulseaudio pkgconf nano stow
 echo "Success"
 
-echo "Installing yay..."
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-echo "Success"
-
 echo "Installing dependencies..."
 yay -S --needed ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git xcb-util-errors hyprutils-git glaze hyprgraphics-git hyprshot
 sudo pacman -S --needed aquamarine hyprlang hyprcursor hyprutils hyprgraphics
